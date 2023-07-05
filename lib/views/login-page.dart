f289e2f4a5a29d2fa695ed42bbf034070e0d4011
Moises Correa <moisesbrambilla@hotmail.com>
2023-07-05 15:12:46 -0300

    testes do funcionamento git

--- lib/views/login-page.dart
+++ lib/views/login-page.dart
@@ -14,29 +14,27 @@ class _LoginPageState extends State<LoginPage> {
         body: Column(
       children: [
         Flexible(
-          flex: 2,
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FittedBox(
-                  fit: BoxFit.contain,
-                  child: Container(
-                    width: 100,
-                    child: Image.network(
-                        "https://img.freepik.com/icones-gratis/mac-os_318-10374.jpg?w=740&t=st=1688499535~exp=1688500135~hmac=18e7a8565d35d76b456c1359c83fb8834ba0a6f27d6b4364c7647d01bf57f22b"),
-                  ),
+          flex: 3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.contain,
+                child: Container(
+                  width: 100,
+                  child: Image.network(
+                      "https://img.freepik.com/icones-gratis/mac-os_318-10374.jpg?w=740&t=st=1688499535~exp=1688500135~hmac=18e7a8565d35d76b456c1359c83fb8834ba0a6f27d6b4364c7647d01bf57f22b"),
                 ),
-                Form(
-                    child: Column(
-                  children: [
-                    TextFormField(),
-                    TextFormField(),
-                  ],
-                ))
-              ],
-            ),
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(),
+                  TextFormField(),
+                ],
+              ))
+            ],
           ),
         ),
         Flexible(

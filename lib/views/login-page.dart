2274693f0076dbfda9851047abfe7d9e5251d3c4
Moises Correa <moisesbrambilla@hotmail.com>
2023-07-05 16:02:47 -0300

    Testes de Layout

--- lib/views/login-page.dart
+++ lib/views/login-page.dart
@@ -11,38 +11,59 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Flexible(
-          flex: 3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FittedBox(
-                fit: BoxFit.contain,
-                child: Container(
-                  width: 100,
-                  child: Image.network(
-                      "https://img.freepik.com/icones-gratis/mac-os_318-10374.jpg?w=740&t=st=1688499535~exp=1688500135~hmac=18e7a8565d35d76b456c1359c83fb8834ba0a6f27d6b4364c7647d01bf57f22b"),
-                ),
+        body: Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FittedBox(
+              fit: BoxFit.contain,
+              child: Container(
+                width: 100,
+                child: Image.network(
+                    "https://img.freepik.com/icones-gratis/mac-os_318-10374.jpg?w=740&t=st=1688499535~exp=1688500135~hmac=18e7a8565d35d76b456c1359c83fb8834ba0a6f27d6b4364c7647d01bf57f22b"),
               ),
-              Form(
-                  child: Column(
+            ),
+            Form(
+                child: Column(
+              children: [
+                TextFormField(),
+                TextFormField(),
+              ],
+            )),
+            Container(child: Text("Esqueceu sua senha?")),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Container(child: Text("ou")),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  TextFormField(),
-                  TextFormField(),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.grey,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.grey,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.grey,
+                  ),
                 ],
-              ))
-            ],
-          ),
+              ),
+            ),
+            Text("Não possui uma conta? Cadastre-se!")
+          ],
         ),
-        Flexible(
-            flex: 1,
-            child: Container(
-              color: Colors.blue,
-            ))
-      ],
+      ),
     ));
   }
 }
